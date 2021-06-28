38cd3615412fa08a2d4fb5e3650c8737193da559
Yilmer Avila <yilmer0001@hotmail.com>
2021-06-28 13:31:37 -0500

    Fix env variables

--- client/src/redux/actionCreators.js
+++ client/src/redux/actionCreators.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
-import dotenv from 'dotenv';
 import * as actionTypes from './actionTypes';
 
-dotenv.config();
-const BASE_URL = 'http://localhost:3001/api/';
 export const instantiateAxios = () =>
   axios.create({
     withCredentials: true,
-    baseURL: process.env.REACT_APP_BACKEND_URL || BASE_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
-      'Access-Control-Allow-Origin': process.env.REACT_APP_DOMAIN || 'localhost:3001',
+      'Access-Control-Allow-Origin': process.env.REACT_APP_DOMAIN,
       Authorization: localStorage.getItem('AUTH_TOKEN'),
     },
   });
